91c7a9d69a1e40f3ce976515597dc423830224ab
anjelicgutiguti <anjelic.may@gmail.com>
2026-03-03 15:12:17 -0500

    save3-3

--- Assets/playercontroller.cs
+++ Assets/playercontroller.cs
@@ -3,23 +3,54 @@ using UnityEngine;
 public class playercontroller : MonoBehaviour
 {
     public float moveSpeed = 5;
-    Rigidbody rb;
-    Vector2 move;
-    
+    Rigidbody2D rb;
+
+    //jump code
+    public float jumpForce = 12;
+    public Transform groundCheck;
+    public float groundcheckRadius = 1;
+    public LayerMask groundlayer;
+
+    //guncall, shoot
+    public Transform firepoint;
+    public pitol gun;
+
+    public bool isGrounded;
+    float moveX;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        move = new Vector2(Input.GetAxisRaw("horizontal"),
-                           Input.GetAxisRaw("vertical")).normalized;
+        moveX = Input.GetAxisRaw("horizontal");
 
-        rb.linearVelocity = move * moveSpeed * Time.deltaTime;
-       
-                          
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundcheckRadius, groundlayer);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+
+        Vector3 mouseWorld = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        mouseWorld.z = 0;
+        Vector2 aimDirection = (mouseWorld - firepoint.position);
+        float angle = Mathf.Atan2(aimDirection.y, aimDirection.x);
+        firepoint.rotation = Quaternion.Euler(0, 0, angle);
+
+       // if (Input.GetMouseButtonDown(0))
+       // {
+        //    pitol.TryShoot();
+       // }
+
     }
+    void FixedUpdate()
+    {
+     rb.linearVelocity = new Vector2 (moveX * moveSpeed, rb.linearVelocity.y);
+    }
+
 }
